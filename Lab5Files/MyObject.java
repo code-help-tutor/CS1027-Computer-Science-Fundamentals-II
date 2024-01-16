c67f9a9d53889fd2a48be54752854802fc1b7476
Code Help Tutor <1823890830@qq.com>
2024-01-16 05:52:15 +0000

    WeChat cstutorcs CS1027 Computer Science Fundamentals II

---
+++ Lab5Files/MyObject.java
@@ -0,0 +1,19 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+
+public class MyObject {
+	private String name;
+	
+	public MyObject(String theName) {
+		name = theName;
+	}
+
+	public void setName(String newName) {
+		name = newName;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# CS1027 Computer Science Fundamentals II
+
+# WeChat: cstutorcs
+
+# QQ: 749389476
+
+# Email: tutorcs@163.com
+
+# Computer Science Tutor
+
+# Programming Code Help
